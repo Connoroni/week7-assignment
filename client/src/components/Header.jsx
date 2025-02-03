52ffce9041b6da25fe9a6cce74e6ed4950ce7d30
Connoroni <mitchell2k2@gmail.com>
2025-02-03 02:00:12 +0000

    added a button to link to the newuser route since that is the only feature that works, button is currently very large

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import createUser from "../assets/create_user.png";
 
 export default function Header() {
   return (
@@ -6,6 +7,12 @@ export default function Header() {
       <h1>
         <Link to={"/"}>WEBSITE TITLE TBD</Link>
       </h1>
+      <Link to="/newuser">
+        <img
+          src={createUser}
+          alt='A symbol showing an outline of a person, representing "Create User"'
+        />
+      </Link>
     </>
   );
 }
