3db2e0faa49225fd6c9d071f9fe282bfa92199d9
Connoroni <mitchell2k2@gmail.com>
2025-02-03 00:45:51 +0000

    layout of Comment, Post, and Feed components with placeholders fand comments detailing next steps

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import createPost from "../assets/create_post.png";
+
 export default function Feed() {
+  // fetch all posts from database, SELECT query grabbing all posts with no conditionals
   return (
     <>
       <h2>this is the feed</h2>
+      <Link to="/post">
+        <img
+          src={createPost}
+          alt='A symbol showing a pen and paper, meaning "Create Post"'
+        />
+      </Link>
+      {/* map through array of posts from database, creating a Post component for each one, pass each one props for the username, title, img, and id (comments probably don't need props here because they're getting fetched within the post component, since it needs a second SELECT query using post_id to select only matching comments) */}
     </>
   );
 }
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,7 +1,16 @@
+import { CommentForm } from "./CommentForm";
+
 export default function Post() {
+  // fetch an array of comments from the database with a SELECT query where post_id =
   return (
     <>
-      <h2>hello</h2>
+      <h2>Placeholder_Post_Title</h2>
+      <img
+        src="https://archive.org/download/placeholder-image/placeholder-image.jpg"
+        alt="Standard placeholder image with a grey symbol representing an image on a pale grey background"
+      />
+      <CommentForm />
+      {/* map through comments array to generate Comment component for each, pass props to each one to give it the text and username from the database */}
     </>
   );
 }
