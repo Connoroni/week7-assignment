ffcff197159ccc0fdcbd10e8a318790c160ff1d5
Connoroni <mitchell2k2@gmail.com>
2025-02-04 09:09:42 +0000

    created new tables to replace old (broken) ones, inserted new dummy data

--- server/seed.js
+++ server/seed.js
@@ -1,56 +1,22 @@
 // Seed file just being used to display my queries that I'm writing in Supabase
 
 //! Creating the Tables
-// CREATE TABLE IF NOT EXISTS users (
-//     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//     username VARCHAR(255)
-//   );
-
-//   CREATE TABLE IF NOT EXISTS posts (
-//     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//     post_title VARCHAR(255),
-//     timestamp TIMESTAMP,
-//     img TEXT,
-//     user_id INT REFERENCES users(id)
-//   );
-
-//   CREATE TABLE IF NOT EXISTS comments (
-//     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//     comment_text VARCHAR(255),
-//     user_id INT REFERENCES users(id),
-//     post_id INT REFERENCES posts(id)
-//   );
+// CREATE TABLE IF NOT EXISTS posts2 (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username VARCHAR(255), timestamp TIMESTAMP, post_title VARCHAR(255), img TEXT);
+// CREATE TABLE IF NOT EXISTS comments2 (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username VARCHAR(255), timestamp TIMESTAMP, comment_text VARCHAR(255), post_id INT REFERENCES posts2(id))
 
 //! Inserting Dummy Data (ran each query separately)
-// INSERT INTO users (username)
-// VALUES ('John Smith');
+// INSERT INTO posts2 (username, timestamp, post_title, img)
+// VALUES ('John Smith', current_timestamp, 'Lovely Orangutan Photo', 'https://www.agoodplace.co.uk/images/tut-tut2.jpg');
+// INSERT INTO posts2 (username, timestamp, post_title, img)
+// VALUES ('Connor', current_timestamp, 'Hull Scenery', 'https://www.planetware.com/photos-large/ENG/england-hull-the-deep.jpg');
 
-// INSERT INTO posts (post_title, timestamp, img, user_id)
-// VALUES ('Lovely Orangutan Photo', current_timestamp, 'https://www.agoodplace.co.uk/images/tut-tut2.jpg', 1);
-
-// INSERT INTO comments (comment_text, user_id, post_id)
-// VALUES ('That is such a cute orangutan!', 1, 1);
-
-// INSERT INTO comments (comment_text, user_id, post_id)
-// VALUES ('Lovely photo, my dude!', 1, 1);
-
-// INSERT INTO comments (comment_text, user_id, post_id)
-// VALUES ('I like monkeys!', 1, 1);
-
-// INSERT INTO comments (comment_text, user_id, post_id)
-// VALUES ('Umm actually, orangutans are apes not monkeys.!', 1, 1);
+// INSERT INTO comments2 (username, timestamp, comment_text, post_id)
+// VALUES ('Tony', current_timestamp, 'That is such a cute orangutan!', 1);
+// INSERT INTO comments2 (username, timestamp, comment_text, post_id)
+// VALUES ('Alison', current_timestamp, 'Lovely photo, my dude', 2);
+// INSERT INTO comments2 (username, timestamp, comment_text, post_id)
+// VALUES ('Monkey_Lover', current_timestamp, 'I like monkeys. :)', 1);
+// INSERT INTO comments2 (username, timestamp, comment_text, post_id)
+// VALUES ('U. R. Rong', current_timestamp, 'Umm actually, orangutans are apes not monkeys...', 1);
 
 //! Selecting
-
-//? -- First one is just practice
-// SELECT posts.post_title, posts.timestamp, posts.img, comments.comment_text FROM posts
-// JOIN comments ON posts.id = comments.post_id;
-
-//? -- Selecting all posts by a user (this should appear on user profile)
-// SELECT users.username, posts.post_title, posts.timestamp, posts.img FROM users
-// JOIN posts ON users.id = posts.user_id;
-
-//? -- Selecting posts, comments, and username to show on feed
-// SELECT users.username, posts.post_title, posts.timestamp, posts.img, comments.comment_text FROM users
-// JOIN posts ON users.id = posts.user_id
-// JOIN comments ON posts.id = comments.post_id;
